5850bd56ba051ac79e5019c72035cfb0ad249475
jason_z <jasonz189@nycstudents.net>
2023-09-21 20:17:55 -0400

    added spacing, calculations for costs and tips

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -2,21 +2,32 @@ import java.util.Scanner;
 public class TipCalculator {
     public static void main(String[]args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group: ");
         int people = scan.nextInt();
         scan.nextLine();
         System.out.print("What's the tip percentage? (0-100):");
-        int tipPercent = scan.nextInt();
+        double tipPercent = scan.nextDouble() / 100;
         scan.nextLine();
+
         double totalCost = 0;
         double cost = 0;
         while (cost != -1) {
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             cost = scan.nextDouble();
             scan.nextLine();
-            totalCost += cost;
+            if (cost != -1) {
+                totalCost += cost;
+            }
         }
+
+        double costWithTip = tipPercent * totalCost + totalCost;
+        System.out.println("----------------------------------");
+        System.out.println("Total bill before tip: " + totalCost);
+        System.out.println("Total percentage : " + tipPercent * 100 + "%");
+        System.out.println("Total tip: $" + tipPercent * totalCost);
+        System.out.println("Total bill with tip : $" + costWithTip);
 
     }
 }
